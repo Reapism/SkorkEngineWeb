ed3e7e6e371fe882c9dd4f941b426cd84333d562
Reapism <reapsprgm@gmail.com>
2019-07-28 20:01:32 -0400

    UpdateService is a class that will check for updates from the DB using a lookup table thats relatively static.

---
+++ SkorkEngine/service/UpdateService.cs
@@ -0,0 +1,21 @@
+﻿namespace SkorkEngine.service {
+    public class UpdateService {
+
+        private string version;
+
+        public UpdateService() {
+            this.version = "";
+        }
+
+        public bool CheckUpdates() {
+
+            long dbVersion = 0;
+            if (long.Parse(version.Replace(".",string.Empty)) < dbVersion) {
+                // update is available!
+            }
+
+
+            return true;
+        }
+    }
+}

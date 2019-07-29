f079b0ad54207c1676fab661a75d6c2fc39bbf46
Reapism <reapsprgm@gmail.com>
2019-07-28 20:00:20 -0400

    Added the SkorkException class which is the root exception class for Skork. SkorkException extends Exception.

---
+++ SkorkEngine/exception/SkorkDrawException.cs
@@ -0,0 +1,12 @@
+﻿namespace SkorkEngine.exception {
+    public class SkorkDrawException : SkorkException {
+
+        public SkorkDrawException(string msg) : base(msg) {
+
+        }
+
+        public override string ToString() {
+            return base.ToString();
+        }
+    }
+}

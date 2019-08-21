cf18b722536708b55022b3e028287a6f725e59ef
Reapism <reapsprgm@gmail.com>
2019-08-21 01:18:45 -0400

    ui

--- SkorkEngine/GUI/MainWindow.xaml.cs
+++ SkorkEngine/GUI/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
+using Skork_Engine_Library.Parse;
+using SkorkEngine.Service;
 
 namespace SkorkEngine.GUI {
     /// <summary>
@@ -7,6 +11,28 @@ namespace SkorkEngine.GUI {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+
+        private void MenuCompile_Click(object sender, RoutedEventArgs e) {
+            int i = CompileService.CompileCode(ref richTextCode);
+            MessageBox.Show(i.ToString());
+        }
+
+        private void MenuClean_Click(object sender, RoutedEventArgs e) {
+            List<string> code = CompileService.CleanCode(ref richTextCode);
+
+            foreach (string s in code) {
+                MessageBox.Show(s);
+            }
+        }
+
+        private void MenuParse_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void MenuCustom_Click(object sender, RoutedEventArgs e) {
+
         }
     }
 }
